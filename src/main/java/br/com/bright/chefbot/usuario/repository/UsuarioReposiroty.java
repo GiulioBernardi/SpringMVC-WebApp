75da4fd8450a335cbcef5cc49b158ef91ad3d878
Giulio Bernardi <giulioccbernardi@gmail.com>
2022-09-05 16:16:24 -0300

    Adicionar landing page - excluir classes inutilizadas - mais

--- src/main/java/br/com/bright/chefbot/usuario/repository/UsuarioReposiroty.java
+++
@@ -1,9 +0,0 @@
-package br.com.bright.chefbot.usuario.repository;
-
-import br.com.bright.chefbot.usuario.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UsuarioReposiroty extends JpaRepository<Usuario, Long> {
-}
